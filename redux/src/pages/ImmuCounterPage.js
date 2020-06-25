a9a3027f1a03d6ed1151cc2c0f517b8dfa9926a2
Hyoungnam Kim <kimhyoungnams@gmail.com>
2020-06-25 13:35:47 +0900

    [redux] delete immutable, temp reselect

--- redux/src/pages/ImmuCounterPage.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import ImmuCounterContainer from 'containers/ImmuCounterContainer';
-
-function ImmuCounterPage  () {
-  return (
-    <ImmuCounterContainer />
-  )
-}
-export default ImmuCounterPage;
