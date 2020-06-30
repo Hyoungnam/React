59c5b16c99a2d7edcefc7569629b42d921978a09
Hyoungnam Kim <kimhyoungnams@gmail.com>
2020-06-30 18:21:12 +0900

    [redux-form] initializeFromStateForm

--- redux-form/src/App.js
+++ redux-form/src/App.js
@@ -1,12 +1,16 @@
 import React from "react";
+// import { Values } from "redux-form-website-template";
 import CounterContainer from "containers/Counter";
 import ContactContainer from "containers/Contact";
+import InitializeFromStateForm from "containers/InitializeFromStateForm";
 
 function App() {
   return (
     <div className="App">
       <CounterContainer />
       <ContactContainer />
+      <InitializeFromStateForm />
+      {/* <Values form="initializeFromState" /> */}
     </div>
   );
 }
--- redux-form/src/redux/index.js
+++ redux-form/src/redux/index.js
@@ -2,12 +2,13 @@ import { createStore, combineReducers, applyMiddleware } from "redux";
 import { reducer as formReducer } from "redux-form";
 import { composeWithDevTools } from "redux-devtools-extension";
 import counter from "./counter";
-
+import account from "./account";
 const rootReducer = combineReducers({
   // ...your other reducers here
   // you have to pass formReducer under 'form' key,
   // for custom keys look up the docs for 'getFormState'
-  counter: counter,
+  account,
+  counter,
   form: formReducer,
 });
 export const store = createStore(
